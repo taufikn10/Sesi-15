6b346063524cb5889b1791bc72125fc90b16fdb2
Taufik Nurrahman <official.taufik980@gmail.com>
2022-03-13 17:01:56 +0700

    update

--- react-router-app/src/components/Header.js
+++ react-router-app/src/components/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Navbar from "./Navbar";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
@@ -7,12 +8,12 @@ function Header() {
       <Navbar />
       <header className="py-3 mb-4 border-bottom">
         <div className="container d-flex flex-warp justify-content-between">
-          <a
-            href="/"
+          <Link
+            to="/"
             className="d-flex align-items-center mb-3 mb-lg-0 me-lg-auto text-dark text-decoration-none"
           >
             <span className="fs-4">My First React App</span>
-          </a>
+          </Link>
         </div>
       </header>
     </>
